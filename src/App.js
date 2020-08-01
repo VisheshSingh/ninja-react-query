2dc84393fc6d513b0461cc2903332d4c77eb3b7f
Vishesh Singh <VisheshSingh16@gmail.com>
2020-08-01 09:45:56 -0500

    devtools 🔧🔩🛠

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import Navbar from './components/Navbar';
 import Planets from './components/Planets';
 import People from './components/People';
@@ -6,13 +7,16 @@ import People from './components/People';
 function App() {
   const [page, setPage] = useState('planets');
   return (
-    <div className='App'>
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className='content'>
-        {page === 'planets' ? <Planets /> : <People />}
+    <>
+      <div className='App'>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className='content'>
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
