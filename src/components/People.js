53a8f66291f76a03847cd1e884a03acb679b66c4
Vishesh Singh <VisheshSingh16@gmail.com>
2020-07-31 22:41:59 -0500

    The useQuery Hook

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+
+import Person from './Person';
+
+const fetchPeople = async () => {
+  const res = await fetch(`https://swapi.dev/api/people`);
+  return res.json();
+};
 
 const People = () => {
+  const { data, status } = useQuery('people', fetchPeople);
+
   return (
     <div>
       <h2>People</h2>
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'error' && <p>Error fetching people</p>}
+      {status === 'success' &&
+        data.results.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
     </div>
   );
 };
